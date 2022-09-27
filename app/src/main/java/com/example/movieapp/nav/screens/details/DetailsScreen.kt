fa8c0e062ba119e80890afeeb8aa2fa952e2f85d
joey sparidaans <jsparidaans@gmail.com>
2022-09-27 11:43:48 +0200

    added movie images to movie details

--- app/src/main/java/com/example/movieapp/nav/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/movieapp/nav/screens/details/DetailsScreen.kt
@@ -2,6 +2,8 @@ package com.example.movieapp.nav.screens.details
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -11,6 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.example.movieapp.model.Movie
 import com.example.movieapp.model.getMovies
 import com.example.movieapp.widgets.MovieRow
 
@@ -35,12 +39,28 @@ fun DetailsScreen(navController: NavController, movieId: String?) {
                 horizontalAlignment = CenterHorizontally,
                 verticalArrangement = Arrangement.Top
             ) {
-                MovieRow(movie=movie)
-                Text(
-                    text = movie.title,
-                    style = MaterialTheme.typography.h5
-                )
+                MovieRow(movie = movie)
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
+                Text(text = "Movie Images")
+                HorizontalScrollableImageView(movie)
 
+            }
+        }
+    }
+}
+
+@Composable
+private fun HorizontalScrollableImageView(movie: Movie) {
+    LazyRow {
+        items(movie.images) { image ->
+            Card(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(240.dp),
+                elevation = 5.dp
+            ) {
+                AsyncImage(model = image, contentDescription = "movie image")
             }
         }
     }
