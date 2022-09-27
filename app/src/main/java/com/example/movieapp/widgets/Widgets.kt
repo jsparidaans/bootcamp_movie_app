ebdbffefae9c9553bf9297e54bfebc859233b74b
joey sparidaans <jsparidaans@gmail.com>
2022-09-27 11:25:54 +0200

    made movie card expandable

--- app/src/main/java/com/example/movieapp/widgets/Widgets.kt
+++ app/src/main/java/com/example/movieapp/widgets/Widgets.kt
@@ -1,23 +1,28 @@
 package com.example.movieapp.widgets
 
-import androidx.compose.foundation.Image
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
 import com.example.movieapp.model.Movie
@@ -26,11 +31,13 @@ import com.example.movieapp.model.getMovies
 @Preview
 @Composable
 fun MovieRow(movie: Movie = getMovies().first(), onClick: (String) -> Unit = {}) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
             .clickable { onClick(movie.id) },
         shape = RoundedCornerShape(CornerSize(16.dp)),
         elevation = 6.dp
@@ -68,8 +75,40 @@ fun MovieRow(movie: Movie = getMovies().first(), onClick: (String) -> Unit = {})
                     text = "Released: ${movie.year}",
                     style = MaterialTheme.typography.caption
                 )
+                AnimatedVisibility(visible = expanded) {
+                    Column() {
+                        Text(text = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontSize = 13.sp
+                                )
+                            ) {
+                                append("Plot: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontSize = 13.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            ) {
+                                append(movie.plot)
+                            }
+                        })
+                    }
+                }
 
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable { expanded = !expanded },
+                    tint = Color.DarkGray,
+                    contentDescription = "expand card"
+                )
             }
         }
+
     }
 }
